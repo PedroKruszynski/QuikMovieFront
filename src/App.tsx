7b8d9acc807012a346edae3ddff6211e2be01953
Pedro Kruszynski <pedro.nascimento39@etec.sp.gov.br>
2020-12-05 15:38:41 -0300

    initial commit and structure of project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
+
+export default App;
